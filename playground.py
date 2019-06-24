a0bb6478554b1c1570a859b4348863d814db05b1
Manikandan Venkatraman <mvenkatraman@jet2.com>
2019-06-24 13:42:43 +0100

    initial version of tamil text classifier

---
+++ playground.py
@@ -0,0 +1,14 @@
+
+#%%
+import numpy as np
+import pandas as pd
+
+Corpus = pd.read_csv("dataset/ta/news_headline_sample.csv", sep="|")
+
+
+#%%
+Corpus = Corpus[Corpus["label"].isnull()]
+Corpus
+
+
+#%%
